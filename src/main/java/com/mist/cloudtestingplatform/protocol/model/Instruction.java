09091f43e440d752b2a9fbe666b25eb6d94c3519
Prophet Xu <slowfishcolor@gmail.com>
2018-01-06 16:04:38 +0800

    UPDATE: update Instruction and InstructionData

--- src/main/java/com/mist/cloudtestingplatform/protocol/model/Instruction.java
+++ src/main/java/com/mist/cloudtestingplatform/protocol/model/Instruction.java
@@ -7,9 +7,104 @@ import java.util.Map;
  */
 public class Instruction {
 
-    private int instructionCount;
+    private String name;
 
-    private String instructionType;
+    private String instruction;
 
-    private Map<String, Instruction> instructionMap;
+    private double value;
+
+    private double[] values;
+
+    private double minValue;
+
+    private double maxValue;
+
+    private boolean pass;
+
+    // 发送指令的 interval
+    private long interval;
+
+    private long timestamp;
+
+    private String remark;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double[] getValues() {
+        return values;
+    }
+
+    public void setValues(double[] values) {
+        this.values = values;
+    }
+
+    public double getMinValue() {
+        return minValue;
+    }
+
+    public void setMinValue(double minValue) {
+        this.minValue = minValue;
+    }
+
+    public double getMaxValue() {
+        return maxValue;
+    }
+
+    public void setMaxValue(double maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    public boolean isPass() {
+        return pass;
+    }
+
+    public void setPass(boolean pass) {
+        this.pass = pass;
+    }
+
+    public long getInterval() {
+        return interval;
+    }
+
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 }
--- src/main/java/com/mist/cloudtestingplatform/protocol/model/InstructionData.java
+++ src/main/java/com/mist/cloudtestingplatform/protocol/model/InstructionData.java
@@ -1,7 +1,49 @@
 package com.mist.cloudtestingplatform.protocol.model;
 
+import java.util.Map;
+
 /**
  * Created by Prophet on 2018/1/5.
  */
-public class InstructionData {
+public class InstructionData extends Data {
+
+    private int instructionCount = 1;
+
+    private Instruction instruction;
+
+    private Instruction[] instructions;
+
+    private Map<String, Instruction> instructionMap;
+
+    public int getInstructionCount() {
+        return instructionCount;
+    }
+
+    public void setInstructionCount(int instructionCount) {
+        this.instructionCount = instructionCount;
+    }
+
+    public Instruction getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(Instruction instruction) {
+        this.instruction = instruction;
+    }
+
+    public Instruction[] getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(Instruction[] instructions) {
+        this.instructions = instructions;
+    }
+
+    public Map<String, Instruction> getInstructionMap() {
+        return instructionMap;
+    }
+
+    public void setInstructionMap(Map<String, Instruction> instructionMap) {
+        this.instructionMap = instructionMap;
+    }
 }
